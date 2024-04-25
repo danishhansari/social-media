9d1b0af7b46c72716d7aadce06c2220156df136b
Danish <dan71ish@gmail.com>
2024-04-25 22:14:56 +0530

    create post ,like post, get post

---
+++ server/route/post.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { verifyToken } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/middleware.js";
+import {
+  createPost,
+  getFeedPost,
+  getUserProfile,
+  likePost,
+} from "../controller/post.controller.js";
+
+const router = Router();
+
+router.post("/", verifyToken, upload.single("picture"), createPost);
+router.get("/", verifyToken, getFeedPost);
+router.get("/:userId/posts", verifyToken, getUserProfile);
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
