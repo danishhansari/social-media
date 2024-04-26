eedbf597ca86c01acf159cfac9b3461f85710af6
Danish <dan71ish@gmail.com>
2024-04-26 12:55:21 +0530

    setted the redux and also navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
-import Navbar from "./pages/Navbar";
-const Home = lazy(() => import("./pages/Home"));
+import { lazy, Suspense, useMemo } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 const Login = lazy(() => import("./pages/Login"));
 const Profile = lazy(() => import("./pages/Profile"));
 import Loader from "./components/Loader";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSetting } from "./theme.js";
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSetting(mode)), [mode]);
+
   return (
     <>
       <Router>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile/:userId" element={<Profile />} />
-          </Routes>
-        </Suspense>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Navbar />}>
+                <Route index element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile/:userId" element={<Profile />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </ThemeProvider>
       </Router>
     </>
   );
