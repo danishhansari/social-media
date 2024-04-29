d2661c2c67d694c9d74d6b98d27d1d9997da499f
Danish <dan71ish@gmail.com>
2024-04-29 11:21:14 +0530

    login ui

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,29 +1,32 @@
 import Twitterlogo from "../components/Twitterlogo";
+import googleLogo from "../assets/google.png";
+
 const AuthPage = () => {
   return (
     <div className="h-cover px-4 max-h-full w-full flex items-center justify-center">
-      <div className="flex flex-col md:flex-row justify-between w-full md:w-[65%] mx-auto items-center">
+      <div className="flex flex-col md:flex-row justify-between w-full md:w-[50%] mx-auto items-center">
         <div className="w-full hidden md:block">
           <Twitterlogo />
         </div>
-        <div>
 
-          <div className="max-w-[100px] md:hidden">
+        <div className="text">
+          <div className="max-w-[70px] mt-4 md:hidden">
             <Twitterlogo />
           </div>
 
-          <h1 className="text-6xl font-bold my-4 md:m-0">Happening now </h1>
-          <h2 className="text-3xl font-bold mb-2 md:my-8">Join today. </h2>
-          <div className="w-full md:w-[70%]">
-            <button className="bg-white border-gray-300 border text-center py-2 rounded-full w-full">
+          <h1 className="text-4xl md:text-6xl my-4 md:m-0">Happening now </h1>
+          <h2 className="text-xl md:text-3xl mb-2 md:my-8">Join today. </h2>
+          <div className="w-full md:w-[80%]">
+            <button className="bg-white border border-gray-100  text-center py-2 rounded-full w-full flex items-center justify-center gap-2 hover:bg-gray-200">
+              <img className="w-5" src={googleLogo} alt="google logo" />
               Sign up with Google
             </button>
             <div className="relative gap-2 my-4 flex items-center">
-              <hr className="w-1/2" />
-              <p>or</p>
-              <hr className="w-1/2" />
+              <hr className="w-1/2 border-gray-50" />
+              <p className="text-gray-200">or</p>
+              <hr className="w-1/2 border-gray-50" />
             </div>
-            <button className="bg-white border-gray-300 border text-center py-2 rounded-full w-full my-2">
+            <button className="text-center py-2 rounded-full w-full my-2 bg-primary text-white font-semibold hover:bg-primary/90 transition-colors">
               Create account
             </button>
             <p className="text-sm">
@@ -32,7 +35,7 @@ const AuthPage = () => {
             </p>
             <div className="mt-8">
               <p>Already have an account</p>
-              <button className="bg-white border-gray-300 border text-center py-2 rounded-full w-full my-2 md:my-4">
+              <button className="md:my-4 text-center py-2 rounded-full w-full my-2 bg-primary text-white font-semibold hover:bg-primary/90 transition-colors">
                 Sign in
               </button>
             </div>
