2028733a13104d6a43b3c943e0fdbf82e7f0249f
Danish <dan71ish@gmail.com>
2024-06-15 18:07:54 +0530

    added the compressor to reduct the backend load

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import middleware from "./middleware/middleware.js";
 import userRoute from "./route/user.route.js";
 import { connectDB } from "./db/connect.db.js";
-import {
-  userProfile,
-} from "./controller/user.controller.js";
+import dotenv from "dotenv";
+import { userProfile } from "./controller/user.controller.js";
 
+dotenv.config();
 const app = express();
 app.use(middleware);
 app.get("/:username", userProfile);
--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,11 +1,13 @@
 import { Router, json } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import compression from "compression";
 
 const middleware = Router();
 middleware.use(json());
 middleware.use(cors());
 middleware.use(cookieParser());
+middleware.use(compression());
 
 // Global catches route for finding potential error
 middleware.use((err, req, res, next) => res.status(500).json(err.message));
